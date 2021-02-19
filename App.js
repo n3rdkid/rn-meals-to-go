1669c757fac7f718cd4ad5b97aebccea38b156c8
Saurav Adhikari <sauravads123@gmail.com>
2021-02-19 13:42:37 +0545

    improv : added eslint and prettier

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-    <StatusBar style="auto"/>
-    <SafeAreaView style={{flex:1}}>
-    <View style={styles.search}>
-     <Text>Search</Text>
-    </View>
-    <View style={styles.list}>
-    <Text>List</Text>
-    </View>
-    </SafeAreaView>
+      <StatusBar style="auto" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   search: {
-    padding:16,
-    backgroundColor:'red'
+    padding: 16,
+    backgroundColor: "red",
   },
   list: {
-    flex:1,
-    padding:16,
-    backgroundColor:'green'
+    flex: 1,
+    padding: 16,
+    backgroundColor: "green",
   },
 });
