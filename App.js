f8eb7c1ea08dcbb5d084f87019bea3422ac84304
Saurav Adhikari <sauravads123@gmail.com>
2021-02-19 13:22:30 +0545

    improv : setup basic layout

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
+    <>
+    <StatusBar style="auto"/>
+    <SafeAreaView style={{flex:1}}>
+    <View style={styles.search}>
+     <Text>Search</Text>
     </View>
+    <View style={styles.list}>
+    <Text>List</Text>
+    </View>
+    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  search: {
+    padding:16,
+    backgroundColor:'red'
+  },
+  list: {
+    flex:1,
+    padding:16,
+    backgroundColor:'green'
   },
 });
